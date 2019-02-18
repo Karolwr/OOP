b19fa8b03545c2f1ef5cff6db395471a9be1597d
Karol <karolwroblewicz@gmail.com>
2019-02-18 14:38:11 +0100

    Car

---
+++ src/controller/CarController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import model.Car;
+
+public class CarController {
+    Car[] cars = new Car[100];
+    int index = 0;
+
+    public void purchaseCar(String brand, String model, double price, int year) {
+        
+    }
+
+    public void getCars() {
+
+    }
+}
