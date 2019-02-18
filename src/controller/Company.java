9d0463c6e9a039b2349b2f0e1d074f17b41b5d10
Karol <karolwroblewicz@gmail.com>
2019-02-18 13:45:39 +0100

    po obiedzie:)

--- src/controller/Company.java
+++ src/controller/Company.java
@@ -14,21 +14,33 @@ public class Company {
             String empl_date,
             double salary_net) {
 // utorzenie obiektu pracownika
-        Employee e = new Employee(name,lastname,PESEL,birh_date,empl_date,salary_net);
+        Employee e = new Employee(name, lastname, PESEL, birh_date, empl_date, salary_net);
+        
 // zapisanie pracownika do listy pracownikow
 
         employee[index] = e;
+
+        // inkrementacja index-u
+        index++;
 
 //wyswietlenie danych zapisanego pracownika
 
         System.out.println("Dodano pracownika: " + e);
     }
 
+    public void getAllEmployees() {
+        for (int i = 0; i < index; i++) {
+            System.out.println(employee[i]);
+        }
+    }
+
     public static void main(String[] args) {
         Company c = new Company();
-        c.addEmployee("M","K","22222222","2000-12-11","2000-12-13", 10000);
-        c.addEmployee("D","S","44444444","2005-12-11","2005-12-13", 12000);
-        c.addEmployee("G","E","55555555","2004-12-11","2012-12-13", 13000);
+        c.addEmployee("M", "K", "22222222", "2000-12-11", "2000-12-13", 10000);
+        c.addEmployee("D", "S", "44444444", "2005-12-11", "2005-12-13", 12000);
+        c.addEmployee("G", "E", "55555555", "2004-12-11", "2012-12-13", 13000);
+
+        c.getAllEmployees();
     }
 }
 
