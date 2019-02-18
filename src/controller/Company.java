dfa03dca9cbb165d134c82b09cfc772975404045
Karol <karolwroblewicz@gmail.com>
2019-02-18 14:20:50 +0100

    po

--- src/controller/Company.java
+++ src/controller/Company.java
@@ -15,7 +15,7 @@ public class Company {
             double salary_net) {
 // utorzenie obiektu pracownika
         Employee e = new Employee(name, lastname, PESEL, birh_date, empl_date, salary_net);
-        
+
 // zapisanie pracownika do listy pracownikow
 
         employee[index] = e;
@@ -34,12 +34,36 @@ public class Company {
         }
     }
 
+    public Employee getEmployeeByPESEL(String PESEL_search) {
+        for (int i = 0; i < index; i++) {
+            System.out.println(employee[i]);
+            if (employee[i].PESEL.equals(PESEL_search)) {
+                return employee[i];
+            }
+        }
+        return null;
+    }
+
+    public void setSalary(String PESEL_search, double newSalary) {
+        for (int i = 0; i < index; i++) {
+            System.out.println(employee[i]);
+            if (employee[i].PESEL.equals(PESEL_search)) {
+                employee[i].salary_net = newSalary;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Company c = new Company();
+        //wywolanie metod
         c.addEmployee("M", "K", "22222222", "2000-12-11", "2000-12-13", 10000);
         c.addEmployee("D", "S", "44444444", "2005-12-11", "2005-12-13", 12000);
         c.addEmployee("G", "E", "55555555", "2004-12-11", "2012-12-13", 13000);
 
+        c.getAllEmployees();
+        System.out.println(c.getEmployeeByPESEL("55555"));
+        System.out.println(c.getEmployeeByPESEL("55555555"));
+        c.setSalary("55555555",20000);
         c.getAllEmployees();
     }
 }
